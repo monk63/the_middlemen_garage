19afee469f834f744949d5539cc3c14442dfd1bf
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-04-16 11:21:39 +0000

    x x

---
+++ lib/pages/splash.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+// Reference : https://github.com/AminullahTajMuhammad/login_ui_design/tree/master/lib
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = const Duration(seconds: 4);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initWidget(context);
+  }
+
+  Widget initWidget(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 14, 67, 167),
+                gradient: LinearGradient(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    colors: [(Color.fromARGB(255, 184, 30, 19)), Color.fromARGB(255, 192, 22, 22)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            // ignore: avoid_unnecessary_containers
+            child: Container(
+              child: Image.asset("assets/images/splash.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
