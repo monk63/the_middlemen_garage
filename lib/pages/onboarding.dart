38802b96edafc463c6e576f2b047ee5944394fd4
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-05-01 20:15:20 +0000

    added onboarding

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,8 +1,10 @@
-import 'package:dots_indicator/dots_indicator.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:hexcolor/hexcolor.dart';
-import '../model/onboarding.dart';
+import 'package:onboarding_screen/onboarding_screen.dart';
+import 'package:the_middlemen_garage/pages/login_screen.dart';
+
+
 
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
@@ -12,157 +14,134 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-  late PageController _controller;
+  final PageController _controller = PageController();
 
-  int currentIndex = 0;
+ 
 
-  @override
-  void initState() {
-    _controller = PageController(initialPage: 0);
-    super.initState();
-  }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
-  // final PageController controller;
+
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: _controller,
-      itemCount: contents.length,
-      onPageChanged: (int index) {
-        setState(() {
-          currentIndex = index;
-        });
-      },
-      itemBuilder: (_, i) {
-        return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: contents[i].gradient,
-            ),
-          ),
-          child: Stack(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: SvgPicture.asset(
-                  'assets/icons/on1.svg',
-                  alignment: Alignment.topCenter,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                top: 60,
-                left: 10,
-                child: SizedBox(
-                  child: SvgPicture.asset(
-                    'assets/icons/on2.svg',
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 10,
-                left: 10,
-                right: 10,
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: DefaultTextStyle(
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        child: Text(contents[i].title),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: DefaultTextStyle(
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          // height: 1.5,
-                        ),
-                        child: Text(
-                          contents[i].description,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10, bottom: 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          DotsIndicator(
-                            dotsCount: contents.length,
-                            position: currentIndex.toDouble(),
-                            decorator: DotsDecorator(
-                              size: const Size.square(9.0),
-                              color: Colors.grey,
-                              spacing: const EdgeInsets.all(3),
-                              sizes: const [
-                                Size(12, 12),
-                                Size(12, 12),
-                                Size(12, 12),
-                              ],
-                              activeSize: const Size(50.0, 9.0),
-                              activeColor: Colors.white,
-                              activeShape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5.0),
-                              ),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              if (currentIndex == contents.length - 1) {
-                                Navigator.restorablePushNamedAndRemoveUntil(
-                                  context,
-                                  "/login",
-                                  (r) => false,
-                                );
-                              }
-                              _controller.nextPage(
-                                duration: const Duration(milliseconds: 200),
-                                curve: Curves.bounceIn,
-                              );
-                            },
-                            child: Row(
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.only(right: 10.0),
-                                  child: DefaultTextStyle(
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                    ),
-                                    child: Text('Next'),
-                                  ),
-                                ),
-                                SvgPicture.asset(
-                                  'assets/icons/on3.svg',
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+    Size size = MediaQuery.of(context).size;
+
+     return OnBoardingScreen(
+      label: const Text(
+        'Get Started',
+        key: Key('get_started'),
+      ),
+
+      /// This function works when you will complete `OnBoarding`
+      function: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => LoginScreen(),
           ),
         );
       },
+
+      /// This [mySlides] must not be more than 5.
+      mySlides: [
+    _SliderModel(
+      imageAssetPath: Image.asset(
+        'assets/images/splash.png',
+        scale: 1,
+      ),
+      title: 'Middlemen Garage',
+      desc: 'discover people and cars',
+      minTitleFontSize: 10,
+      descStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+      titleStyle: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+    ),
+    _SliderModel(
+      imageAssetPath: SvgPicture.asset('assets/icons/s1.svg',
+      width: size.width * 0.8,
+      height: size.height *0.5,
+      fit: BoxFit.fitHeight,
+      ),
+      title: 'Discover new cars',
+      desc: 'discover people',
+    ),
+    _SliderModel(
+      imageAssetPath: SvgPicture.asset('assets/icons/s2.svg',
+      
+      width: size.width * 0.8,
+      height: size.height *0.5,
+      fit: BoxFit.fitHeight,),
+      title: 'Discover new cars',
+      desc: 'discover people',
+    ),
+    _SliderModel(
+      imageAssetPath: SvgPicture.asset('assets/icons/s3.svg',
+      
+      width: size.width * 0.8,
+      height: size.height *0.5,
+      fit: BoxFit.fitHeight,),
+      title: 'Discover new cars',
+      desc: 'discover people',
+    ),
+    _SliderModel(
+      imageAssetPath: SvgPicture.asset('assets/icons/s1.svg',
+      
+      width: size.width * 0.8,
+      height: size.height *0.5,
+      fit: BoxFit.fitHeight,),
+      title: 'Developer cars',
+      desc: 'discover people',
+    ),
+  ],
+      controller: _controller,
+      slideIndex: 0,
+      statusBarColor: Colors.white,
+      indicators: Indicators.cool,
+      skipPosition: SkipPosition.bottomRight,
+      skipDecoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      skipStyle: TextStyle(color: Colors.white),
+
+      pageIndicatorColorList: [
+        Colors.yellow,
+        Colors.green,
+        Colors.red,
+        Colors.yellow,
+        Colors.blue
+      ],
     );
   }
+    
+
+  
+ 
+
+}
+
+class _SliderModel {
+  const _SliderModel({
+    this.imageAssetPath,
+    this.title = "title",
+    this.desc = "title",
+    this.miniDescFontSize = 12.0,
+    this.minTitleFontSize = 15.0,
+    this.descStyle,
+    this.titleStyle,
+  });
+
+  final Widget? imageAssetPath;
+  final String title;
+  final TextStyle? titleStyle;
+  final double minTitleFontSize;
+  final String desc;
+  final TextStyle? descStyle;
+  final double miniDescFontSize;
 }
